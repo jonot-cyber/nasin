780c7b89acdf1151ef713b827f01350f19ef2af3
Joseph Tessitore <josephtessitore@protonmail.com>
2024-05-17 19:53:07 -0400

    Added a pause button
    You can pause tasks now

--- src/main.rs
+++ src/main.rs
@@ -2,58 +2,74 @@ mod scheduler;
 
 use crate::scheduler::{Task, Tasks};
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 use libadwaita::prelude::*;
 
-use libadwaita::{ActionRow, Application, ApplicationWindow, HeaderBar, EntryRow};
-use gtk::{ListBox, Orientation, SelectionMode, Button, ScrolledWindow};
 use gtk::glib;
+use gtk::{Button, ListBox, Orientation, ScrolledWindow, SelectionMode};
+use libadwaita::{ActionRow, Application, ApplicationWindow, EntryRow, HeaderBar};
 
 fn create_row(task: &Task, list: &ListBox, tasks: Rc<RefCell<Tasks>>) -> ActionRow {
     let row = ActionRow::builder()
         .title(glib::markup_escape_text(&task.name))
         .subtitle(format!("Priority: {}", task.priority))
-	.build();
+        .build();
+    let pause_button = Button::builder()
+        .icon_name(if task.paused {
+            "media-playback-start"
+        } else {
+            "media-playback-pause"
+        })
+        .css_classes(vec!["flat"])
+        .build();
     let button = Button::builder()
         .icon_name("edit-delete")
         .css_classes(vec!["flat"])
         .build();
+    row.add_suffix(&pause_button);
     row.add_suffix(&button);
-    button.connect_clicked(glib::clone!(@strong task, @strong tasks, @weak list => move |_| {
-	tasks.borrow_mut().remove(task.clone());
-	build_list_from_tasks(&list, tasks.clone());
-    }));
+    pause_button.connect_clicked(
+        glib::clone!(@strong task, @strong tasks, @weak list => move |_| {
+            tasks.borrow_mut().toggle_pause(&task);
+            build_list_from_tasks(&list, tasks.clone());
+        })
+    );
+    button.connect_clicked(
+        glib::clone!(@strong task, @strong tasks, @weak list => move |_| {
+            tasks.borrow_mut().remove(task.clone());
+            build_list_from_tasks(&list, tasks.clone());
+        }),
+    );
     row
 }
 
 fn build_list_from_tasks(list: &ListBox, tasks: Rc<RefCell<Tasks>>) {
     list.remove_all();
     for task in &tasks.borrow().tasks {
-	list.append(&create_row(task, list, tasks.clone()));
+        list.append(&create_row(task, list, tasks.clone()));
     }
 }
 
 fn build_ui(app: &Application) {
     let tasks = Rc::new(RefCell::new(Tasks::load()));
-    
+
     let list = ListBox::builder()
-	.margin_top(32)
-	.margin_end(32)
-	.margin_bottom(32)
-	.margin_start(32)
-	.selection_mode(SelectionMode::None)
-	.css_classes(vec!["boxed-list"])
-	.build();
+        .margin_top(32)
+        .margin_end(32)
+        .margin_bottom(32)
+        .margin_start(32)
+        .selection_mode(SelectionMode::None)
+        .css_classes(vec!["boxed-list"])
+        .build();
     build_list_from_tasks(&list, tasks.clone());
 
     let add_button = Button::builder().icon_name("list-add").build();
     let step_button = Button::builder().icon_name("edit-redo").build();
     let step_and_finish_button = Button::builder().icon_name("emblem-ok").build();
-    
-    let header_bar = HeaderBar::builder()
-        .build();
+
+    let header_bar = HeaderBar::builder().build();
     header_bar.pack_start(&add_button);
     header_bar.pack_start(&step_button);
     header_bar.pack_start(&step_and_finish_button);
@@ -69,32 +85,30 @@ fn build_ui(app: &Application) {
     content.append(&viewport);
 
     let window = ApplicationWindow::builder()
-	.application(app)
-	.title("Nasin")
-	.content(&content)
-	.build();
-
-    let add_task_title_row = EntryRow::builder()
-        .title("Name")
+        .application(app)
+        .title("Nasin")
+        .content(&content)
         .build();
+
+    let add_task_title_row = EntryRow::builder().title("Name").build();
     let add_task_priority_row = EntryRow::builder()
-	.title("Priority")
-	.input_purpose(gtk::InputPurpose::Digits)
-	.build();
+        .title("Priority")
+        .input_purpose(gtk::InputPurpose::Digits)
+        .build();
     let add_task_create_button = Button::builder()
         .label("Create Task")
         .margin_bottom(12)
         .margin_start(12)
         .css_classes(vec!["suggested-action"])
         .margin_end(12)
-	.build();
+        .build();
     let add_task_content = gtk::ListBox::builder()
         .margin_top(12)
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
         .css_classes(vec!["boxed-list"])
-	.build();
+        .build();
     add_task_content.append(&add_task_title_row);
     add_task_content.append(&add_task_priority_row);
 
@@ -112,38 +126,38 @@ fn build_ui(app: &Application) {
         .build();
 
     add_button.connect_clicked(glib::clone!(@weak add_task_window => move |_| {
-	add_task_window.present();
+    add_task_window.present();
     }));
 
     step_button.connect_clicked(glib::clone!(@strong tasks, @weak list => move |_| {
-	tasks.borrow_mut().step();
-	build_list_from_tasks(&list, tasks.clone());
+    tasks.borrow_mut().step();
+    build_list_from_tasks(&list, tasks.clone());
     }));
 
     step_and_finish_button.connect_clicked(glib::clone!(@strong tasks, @weak list => move |_| {
-	tasks.borrow_mut().step_and_finish();
-	build_list_from_tasks(&list, tasks.clone())
+    tasks.borrow_mut().step_and_finish();
+    build_list_from_tasks(&list, tasks.clone())
     }));
 
     add_task_create_button.connect_clicked(move |_| {
-	let name: String = add_task_title_row.text().to_string();
-	let priority: u8 = add_task_priority_row.text().parse().unwrap_or(1);
-	if priority >= 1 {
-	    tasks.borrow_mut().add(Task::new(name, priority));
-	    build_list_from_tasks(&list, tasks.clone());
-	    add_task_window.close();
-	}
-	// Reset the text of the rows
-	add_task_title_row.set_text("");
-	add_task_priority_row.set_text("");
+        let name: String = add_task_title_row.text().to_string();
+        let priority: u8 = add_task_priority_row.text().parse().unwrap_or(1);
+        if priority >= 1 {
+            tasks.borrow_mut().add(Task::new(name, priority));
+            build_list_from_tasks(&list, tasks.clone());
+            add_task_window.close();
+        }
+        // Reset the text of the rows
+        add_task_title_row.set_text("");
+        add_task_priority_row.set_text("");
     });
     window.present();
 }
 
 fn main() {
     let application = Application::builder()
-	.application_id("me.jonot.Nasin")
-	.build();
+        .application_id("me.jonot.Nasin")
+        .build();
 
     application.connect_activate(build_ui);
 
--- src/scheduler.rs
+++ src/scheduler.rs
@@ -6,12 +6,12 @@ fn get_data_path() -> PathBuf {
     let mut path = dirs::data_dir().unwrap();
     path.push("nasin");
     if !path.exists() {
-	std::fs::create_dir_all(path.clone()).unwrap();
+        std::fs::create_dir_all(path.clone()).unwrap();
     }
     path.push("tasks");
     path.set_extension("json");
     if !path.exists() {
-	File::create(path.clone()).unwrap();
+        File::create(path.clone()).unwrap();
     }
     path
 }
@@ -20,40 +20,44 @@ fn get_data_path() -> PathBuf {
 pub struct Task {
     pub name: String,
     pub priority: u8,
+    pub paused: bool,
     age: u32,
     base_priority: u8,
 }
 
 impl Task {
     pub fn new(name: String, priority: u8) -> Task {
-	Task {
-	    name,
-	    priority,
-	    base_priority: priority,
-	    age: 0,
-	}
+        Task {
+            name,
+            priority,
+            paused: false,
+            base_priority: priority,
+            age: 0,
+        }
     }
 
     pub fn age(&mut self) {
-	self.age = 0;
-	self.priority = std::cmp::max(self.priority - 1, 1);
+        if !self.paused {
+            self.age = 0;
+            self.priority = std::cmp::max(self.priority - 1, 1);
+        }
     }
 
     pub fn reset(&mut self) {
-	self.age = 0;
-	self.priority = self.base_priority;
+        self.age = 0;
+        self.priority = self.base_priority;
     }
 }
 
 impl PartialEq for Task {
     fn eq(&self, other: &Self) -> bool {
-        self.name == other.name && self.age == other.age && self.priority == other.priority && self.base_priority == other.base_priority
+        self.paused == other.paused && self.name == other.name && self.age == other.age && self.priority == other.priority && self.base_priority == other.base_priority
     }
 }
 
 impl PartialOrd for Task {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-	Some(self.cmp(other))
+        Some(self.cmp(other))
     }
 }
 
@@ -61,10 +65,10 @@ impl Eq for Task {}
 
 impl Ord for Task {
     fn cmp(&self, other: &Self) -> Ordering {
-	match self.priority.cmp(&other.priority) {
-	    Ordering::Equal => other.age.cmp(&self.age),
-	    x => x
-	}
+        match self.priority.cmp(&other.priority) {
+            Ordering::Equal => other.age.cmp(&self.age),
+            x => x
+        }
     }
 }
 
@@ -75,100 +79,110 @@ pub struct Tasks {
 
 impl Tasks {
     pub fn new() -> Self {
-	Tasks {
-	    tasks: Vec::new()
-	}
+        Tasks {
+            tasks: Vec::new()
+        }
     }
 
     pub fn load() -> Self {
-	let path = get_data_path();
-	let file = File::options()
-	    .read(true)
-	    .open(path)
-	    .unwrap();
-	if file.metadata().unwrap().len() == 0 {
-	    Tasks::new()
-	} else {
-	    serde_json::from_reader(file).unwrap()
-	}
+        let path = get_data_path();
+        let file = File::options()
+            .read(true)
+            .open(path)
+            .unwrap();
+        if file.metadata().unwrap().len() == 0 {
+            Tasks::new()
+        } else {
+            serde_json::from_reader(file).unwrap()
+        }
     }
 
     fn save(&self) {
-	let path = get_data_path();
-	let file = File::options()
-	    .write(true)
-	    .truncate(true)
-	    .open(path)
-	    .unwrap();
-	serde_json::to_writer(file, self).unwrap();
+        let path = get_data_path();
+        let file = File::options()
+            .write(true)
+            .truncate(true)
+            .open(path)
+            .unwrap();
+        serde_json::to_writer(file, self).unwrap();
     }
 
     pub fn step(&mut self) {
-	// Nothing to do if the list is empty
-	if self.tasks.is_empty() {
-	    return
-	}
-	self.tasks.sort();
-	let mut current_task = self.tasks.remove(0);
-	for task in self.tasks.iter_mut() {
-	    task.age += 1
-	}
-	let oldest_task = if self.tasks.len() == 1 {
-	    &mut self.tasks[0]
-	} else {
-	    self.tasks
-		.iter_mut()
-		.max_by_key(|x| x.age)
-		.unwrap()
-	};
-	oldest_task.age();
-	current_task.reset();
-	self.tasks.push(current_task);
-	self.tasks.sort();
-	// Save after stepping
-	self.save();
+        // Nothing to do if the list is empty
+        if self.tasks.is_empty() {
+            return
+        }
+        self.tasks.sort();
+        let mut current_task = self.tasks.remove(0);
+        for task in self.tasks.iter_mut() {
+            task.age += 1
+        }
+        let oldest_task = if self.tasks.len() == 1 {
+            &mut self.tasks[0]
+        } else {
+            self.tasks
+                .iter_mut()
+                .filter(|x| !x.paused)
+                .max_by_key(|x| x.age)
+                .unwrap()
+        };
+        oldest_task.age();
+        current_task.reset();
+        self.tasks.push(current_task);
+        self.tasks.sort();
+        // Save after stepping
+        self.save();
     }
 
     pub fn step_and_finish(&mut self) {
-	// Nothing to do if the list is empty
-	if self.tasks.is_empty() {
-	    return
-	}
-	self.tasks.sort();
-	self.tasks.remove(0);
-	// Don't do anything else if we removed the last element
-	if self.tasks.is_empty() {
-	    self.save();
-	    return;
-	}
-	for task in self.tasks.iter_mut() {
-	    task.age += 1
-	}
-	let oldest_task = self.tasks.iter_mut().max_by_key(|x| x.age).unwrap();
-	oldest_task.age();
-	self.tasks.sort();
-	// Save after stepping
-	self.save();
+        // Nothing to do if the list is empty
+        if self.tasks.is_empty() {
+            return
+        }
+        self.tasks.sort();
+        self.tasks.remove(0);
+        // Don't do anything else if we removed the last element
+        if self.tasks.is_empty() {
+            self.save();
+            return;
+        }
+        for task in self.tasks.iter_mut() {
+            task.age += 1
+        }
+        let oldest_task = self.tasks.iter_mut().max_by_key(|x| x.age).unwrap();
+        oldest_task.age();
+        self.tasks.sort();
+        // Save after stepping
+        self.save();
     }
 
     pub fn remove(&mut self, task: Task) {
-	for (i, i_task) in self.tasks.iter().enumerate() {
-	    if task == *i_task {
-		self.tasks.remove(i);
-		self.save();
-		return;
-	    }
-	}
-	// Save, but don't bother if we don't have anything here
-	if self.tasks.is_empty() {
-	    self.save();
-	}
+        for (i, i_task) in self.tasks.iter().enumerate() {
+            if task == *i_task {
+            self.tasks.remove(i);
+            self.save();
+            return;
+            }
+        }
+        // Save, but don't bother if we don't have anything here
+        if self.tasks.is_empty() {
+            self.save();
+        }
     }
 
     pub fn add(&mut self, task: Task) {
-	self.tasks.push(task);
-	self.tasks.sort();
-	// Save after adding
-	self.save();
+        self.tasks.push(task);
+        self.tasks.sort();
+        // Save after adding
+        self.save();
+    }
+
+    pub fn toggle_pause(&mut self, task: &Task) {
+        for t in &mut self.tasks {
+            if *task == *t {
+                t.paused = !t.paused
+            }
+        }
+        self.save();
     }
 }
