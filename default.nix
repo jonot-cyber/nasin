78e74b981108a917a85cf78ffd6284a3044cd5bd
Joseph Tessitore <josephtessitore@protonmail.com>
2025-04-27 09:13:09 -0400

    Add flake support

---
+++ default.nix
@@ -0,0 +1,42 @@
+{
+  rustPlatform,
+  gtk4,
+  libadwaita,
+  pkg-config,
+  fetchFromGitHub,
+  hicolor-icon-theme,
+  wrapGAppsHook4,
+  copyDesktopItems,
+  makeDesktopItem
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "nasin";
+  version = "1.0.2";
+
+  src = fetchFromGitHub {
+    owner = "jonot-cyber";
+    repo = pname;
+    rev = "master";
+    hash = "sha256-CPSajEFKJIvJe1G2u5oev7SCepZJ+1JlxDZkNhHlPlY=";
+  };
+
+  buildInputs = [ gtk4 libadwaita ];
+
+  nativeBuildInputs = [ pkg-config hicolor-icon-theme wrapGAppsHook4 copyDesktopItems ];
+  cargoHash = "sha256-zUNbdFTRYtwmyDonScdjJjlFu3pG3dvr9gamYiWek3c=";
+
+  desktopItems = [(makeDesktopItem {
+    name = "nasin";
+    desktopName = "Nasin";
+    terminal = false;
+    categories = [ "Office" ];
+    icon = "me.jonot.Nasin";
+    exec = "nasin";
+  })];
+  
+  postInstall = ''
+    mkdir -p $out/share/icons
+    cp $src/data/me.jonot.Nasin.desktop $out/share/me.jonot.Nasin.desktop
+    cp $src/data/icons/me.jonot.Nasin.svg $out/share/icons/me.jonot.Nasin.svg
+  '';
+}
